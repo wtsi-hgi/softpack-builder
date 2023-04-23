55a264fe7625adbcf70827b4b24fb1795b6b35fa
Altaf Ali <7231912+altaf-ali@users.noreply.github.com>
2023-04-23 19:07:40 +0100

    🚨 fix(test_environment.py): remove unused fixture 'prefect_agent' and commented out code ✨ feat(test_environment.py): remove unused fixture 'prefect_agent' from test signature

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -30,18 +30,17 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize(fixture, param)
 
 
-def test_environment_create_api(prefect_agent, client, spec) -> None:
+def test_environment_create_api(client, spec) -> None:
     model = Environment.Model.from_yaml(spec)
     response = client.post(
         app.url(EnvironmentAPI.url("create")), json=model.dict()
     )
     assert response.status_code == httpx.codes.OK
-    prefect_agent.join(PREFECT_AGENT_TIMEOUT)
+    # prefect_agent.join(PREFECT_AGENT_TIMEOUT)
 
 
-def test_environment_create_command(capsys, service_thread, cli, spec) -> None:
+def test_environment_create_command(service_thread, cli, spec) -> None:
     response = cli.invoke([EnvironmentAPI.name, "create", spec])
-    capsys.readouterr()
     result = Box(yaml.safe_load(response.stdout))
     assert result.state.type == "SCHEDULED"
 
